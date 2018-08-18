1ec8308469077297ccfce1b13a98219393f4207d
Alok Ranjan <alok.ranjan@walkingtree.tech>
2018-08-18 22:40:35 +0530

    Fixed hard coded value in the deployment script.

--- migrations/3_store.js
+++ migrations/3_store.js
@@ -1,5 +1,5 @@
 var Store = artifacts.require("./Store.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Store, "SuperAdmin Store", "This shall never be used by anyone!", 0, "0xf928ac0ed0d0e91f51287a5f28dbdb1019fbba55");
+  deployer.deploy(Store, "SuperAdmin Store", "This shall never be used by anyone!", 0, "");
 };
