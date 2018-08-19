a5c9c73ae572f0c79ce7a2891518b9baef51973c
Alok Ranjan <alok.ranjan@walkingtree.tech>
2018-08-19 19:11:54 +0530

    Initial File for Angular Project

--- contracts/EIP20Interface.sol
+++ contracts/EIP20Interface.sol
@@ -1,39 +1,34 @@
 pragma solidity ^0.4.22;
-// copied from https://github.com/ConsenSys/Tokens/blob/master/contracts/eip20/EIP20Interface.sol
+
+/**
+* Copied from https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/token/ERC20/ERC20.sol
+*/
 
 contract EIP20Interface {
-    /// total amount of tokens
-    uint256 public totalSupply;
+  function totalSupply() public view returns (uint256);
 
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
-    function balanceOf(address _owner) public view returns (uint256 balance);
+  function balanceOf(address _who) public view returns (uint256);
 
-    /// @notice send `_value` token to `_to` from `msg.sender`
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transfer(address _to, uint256 _value) public returns (bool success);
+  function allowance(address _owner, address _spender)
+    public view returns (uint256);
 
-    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-    /// @param _from The address of the sender
-    /// @param _to The address of the recipient
-    /// @param _value The amount of token to be transferred
-    /// @return Whether the transfer was successful or not
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+  function transfer(address _to, uint256 _value) public returns (bool);
 
-    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @param _value The amount of tokens to be approved for transfer
-    /// @return Whether the approval was successful or not
-    function approve(address _spender, uint256 _value) public returns (bool success);
+  function approve(address _spender, uint256 _value)
+    public returns (bool);
 
-    /// @param _owner The address of the account owning tokens
-    /// @param _spender The address of the account able to transfer the tokens
-    /// @return Amount of remaining tokens allowed to spent
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+  function transferFrom(address _from, address _to, uint256 _value)
+    public returns (bool);
 
-    // solhint-disable-next-line no-simple-event-func-name
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 value
+  );
+
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
 }
